9145335796187d9f0bfdee06bcae65b4eea1b2ed
Eric Davenport <EricDavenport@pursuit.org>
2020-02-06 10:50:19 -0500

    Initial Commit

---
+++ TabBarController/TopStoriesTabController.swift
@@ -0,0 +1,23 @@
+//
+//  TopStoriesTabController.swift
+//  NYTTopStories
+//
+//  Created by Eric Davenport on 2/6/20.
+//  Copyright © 2020 Eric Davenport. All rights reserved.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      view.backgroundColor = .green
+      
+    }
+    
+
+
+
+}
